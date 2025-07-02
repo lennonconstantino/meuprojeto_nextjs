3389794d858b13b1967845163f419a527492d5c3
lennonconstantino <lennonconstantino@gmail.com>
2025-07-02 16:03:32 -0300

    knowing server components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,41 @@
-// "use client" // componente client side
-// import {useState} from 'react'
-export default function Home() {
-  //const [user, setUser] = useState("Lennonx")
 
-  //console.log("Lennonx Teste");
+// criando uma tipagem
+interface DataProps {
+  id: number;
+  name: string;
+  full_name: string;
+  owner: {
+    login: string;
+    id: number;
+    avatar_url: string;
+    url: string;
+  }
+}
 
+async function getData() {
+  // https://api.github.com/users/devfraga/repos
+  // const response = await fetch("https://api.github.com/users/devfraga/repos");
+  const response = await fetch("https://api.github.com/users/lennonconstantino/repos");
+  return response.json();
+}
+
+export default async function Home() {
+  // nao precisamos usar o useEffect
+  const data: DataProps[] = await getData();
+  console.log(data);
   return(
     <main>
       <h1>Pagina Home</h1>
       <span>Seja bem vindo a minha pagina home</span>
-
-      {/* <h2>Usuario: {user}</h2> */}
+      <br/>
+      <h3> Meus repositorios</h3>
+      {data.map((item) => (
+        <div key={item.id}>
+          <strong>Repositorio: </strong><a>{item.name}</a>
+          <br />
+          <br />
+        </div>
+      ))}
     </main>
   )
 }
