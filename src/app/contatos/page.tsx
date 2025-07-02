500b8df0e66fabbfab8c2125d1a56932b06f79cb
lennonconstantino <lennonconstantino@gmail.com>
2025-07-02 15:25:05 -0300

    init project

---
+++ src/app/contatos/page.tsx
@@ -0,0 +1,8 @@
+export default function Contatos() {
+    return(
+        <div>
+            <h1>Pagina de contatos</h1>
+            <span>(xx) 0000-0000</span>
+        </div>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+// "use client" // componente client side
+// import {useState} from 'react'
+export default function Home() {
+  //const [user, setUser] = useState("Lennonx")
+
+  //console.log("Lennonx Teste");
+
+  return(
+    <main>
+      <h1>Pagina Home</h1>
+      <span>Seja bem vindo a minha pagina home</span>
+
+      {/* <h2>Usuario: {user}</h2> */}
+    </main>
+  )
+}
